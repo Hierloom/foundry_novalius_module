2fb73ac3d77f0e345db8bf3915018be709925381
Niclas Hilmersson <40474518+Hierloom@users.noreply.github.com>
2024-07-06 18:35:20 +0200

    added log

--- scripts/module.js
+++ scripts/module.js
@@ -3,7 +3,7 @@ console.log("Novalius Module Loaded");
 Hooks.on("setup", () => {
 
     CONFIG.DND5E.spellSchools.growth = "Growth";
-
+    console.log(CONFIG.DND5E.spellSchools.growth, 'added to spell schools.');
 });
 
 console.log(CONFIG.DND5E.spellSchools.growth, 'added to spell schools.');
